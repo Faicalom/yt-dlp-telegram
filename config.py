8832ce4c51d3276876f0ad451bfbb8ea82723c73
Faicalom <faycalom2002@gmail.com>
2026-04-16 17:15:38 +0100

    Update token and secret_key retrieval methods

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
-token: str = os.environ.get("8576952079:AAEPXsaVcLX5oXK0T6VUX8Jzgi3mlxIXRwo")
+token: str = os.environ.get("TOKEN")
 
 logs: int | None = None
 max_filesize: int = 50000000
 output_folder: str = "/tmp/satoru"
 
 allowed_domains: list[str] = ["*"]
-secret_key: str = "any-secret-you-like"
+secret_key: str = os.environ.get("SECRET_KEY", "any-secret-you-like")
 js_runtime: dict | None = {"node": {"path": "node"}}
